a0d88b3ba3295f1b31474f682c9446f87889faa0
Abdullayev-Sarvar <sarvar.abdullayev.0606@gmail.com>
2024-08-28 18:59:31 +0500

    upd

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,51 +1,57 @@
+import React, { useEffect } from 'react';
 import { Container } from '../../utils/Container';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Banner = () => {
+    const data = useSelector((state) => state.searchData);
+    console.log(data);
 
-    const data = useSelector(state=>state.searchData);
-    console.log(data)
-
-  return (
-  <Container>
-  <div className="bg-banner bg-cover bg-center rounded-[20px] h-[500px] mt-[50px] overflow-hidden">
-    <div className="bg-linear-color w-full h-full flex justify-between items-end p-8">
-      {data && (
+   
+    return (
         <>
-          <div className="text-left">
-          <img src={data.current.condition.icon} alt="Weather Icon" className="w-[60px] h-[60px]" />
-
-            <p className="text-[120px] text-white">
-              {data.current.temp_c}°
-            </p>
-            <h1 className="text-[22px] text-white">
-              {data.location.country}, {data.location.region}
-            </h1>
-          </div>
-
-          <div className="text-right ">
-            <div className='flex gap-4'>
-            <p className="text-[24px] text-white">
-              {data.current.condition.text},
-            </p>
-            <p className="text-[24px] text-white">
-              {new Date(data.location.localtime).toLocaleDateString('en-US', {
-                weekday: 'long',
-              })}
-            </p>
-            </div>
-            <p className="text-[24px] text-white">
-              {data.location.localtime}
-            </p>
-          </div>
+            <Container>
+                <div className='bg-banner bg-cover bg-center rounded-lg h-[450px] mt-[50px]   style={{ boxShadow: "0 4px 15px blueviolet" }}'>
+                    <div className='bg-linear-color w-full h-full '>
+                        {data && ( 
+                            <>
+                                <div className='flex justify-between items-center p-4 '>
+                                    <div className='mt-[100px]'>
+                                        <h2>
+                                            <img
+                                                className='w-[100px]'
+                                                src={data.current.condition.icon}
+                                                alt="Weather Icon"
+                                            />
+                                        </h2>
+                                        <h2 className='text-white text-9xl font-semibold'>
+                                            {data.current.temp_c}°C
+                                        </h2>
+                                        <br />
+                                        <div className='flex gap-4 items-center ml-4 '>
+                                            <h1 className='text-white text-4xl font-bold'>
+                                                {data.location.country},
+                                            </h1>
+                                            <h1 className='text-white text-4xl font-bold'>
+                                                {data.location.name}
+                                            </h1>
+                                        </div>
+                                    </div>
+                                    <div className='mt-[300px] ml-4'>
+                                        <h1 className='text-white text-2xl font-bold'>
+                                            {new Date(data.location.localtime).toLocaleTimeString()}
+                                        </h1>
+                                        <h2 className='text-white text-lg'>
+                                            {new Date(data.location.localtime).toLocaleDateString()}
+                                        </h2>
+                                    </div>
+                                </div>
+                            </>
+                        )}
+                    </div>
+                </div>
+            </Container>
         </>
-      )}
-    </div>
-  </div>
-  
-</Container>
+    );
+};
 
-  )
-}
-
-export default Banner
+export default Banner;
